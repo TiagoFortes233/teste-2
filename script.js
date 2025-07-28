77b05006ae25a4c57a7f7ff7c4b92a0364360097
TiagoFortes233 <111051814+TiagoFortes233@users.noreply.github.com>
2025-07-28 13:05:20 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,15 +1,8 @@
-let carrinho = 0;
-function adicionarCarrinho() {
-  carrinho++;
-  document.getElementById("cart-count").innerText = carrinho;
-  localStorage.setItem("carrinho", carrinho);
+function toggleMenu() {
+    const menu = document.getElementById('side-menu');
+    if (menu.style.display === 'flex') {
+        menu.style.display = 'none';
+    } else {
+        menu.style.display = 'flex';
+    }
 }
-window.onload = () => {
-  const guardado = localStorage.getItem("carrinho");
-  if (guardado) document.getElementById("cart-count").innerText = guardado;
-
-  document.getElementById("menu-toggle").addEventListener("click", () => {
-    const menu = document.getElementById("side-menu");
-    menu.classList.toggle("hidden");
-  });
-};
